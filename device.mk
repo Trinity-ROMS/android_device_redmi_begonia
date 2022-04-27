19b0fe20f8342cffbc16189aa1edb17922984bac
LuK1337 <priv.luk@gmail.com>
2022-04-27 23:25:22 +0530

    begonia: init: Add init to handle dalvik props based on sysram
    Signed-off-by: 7Soldier <reg.fm4@gmail.com>
    Signed-off-by: Bishwo <bishwobhusal75@gmail.com>

--- device.mk
+++ device.mk
@@ -10,9 +10,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
