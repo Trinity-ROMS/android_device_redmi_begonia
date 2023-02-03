1260f03eba0c5d81a935db3b1462b02ddb56fa7c
bengris32 <bengris32@protonmail.ch>
2023-02-03 13:19:24 +0300

    begonia: Switch to common Vibrator AIDL * Enable back vibration adjustment support while at it.
    Co-authored-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I6c446e8c05c9ea970090ce6ae3aa041cc2fe633a
    Signed-off-by: MrErenK <akbaseren4751@gmail.com>
    Signed-off-by: SHADOW <ayush12345.as@gmail.com>

--- device.mk
+++ device.mk
@@ -385,7 +385,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator-service.begonia
+    android.hardware.vibrator-service.mediatek
 
 # VNDK
 PRODUCT_COPY_FILES += \
