cb4a5cc55621643212f62acbffa025db73e3ffcf
Saikrishna1504 <saikrishna26918@gmail.com>
2023-09-22 16:01:36 +0530

    begonia: build ANX Camera
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- aosp_begonia.mk
+++ aosp_begonia.mk
@@ -20,6 +20,11 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit some extras stuff
 $(call inherit-product-if-exists, vendor/extras/extras.mk)
+$(call inherit-product-if-exists, vendor/ANXCamera/config.mk)
+
+# Fix uses broken libraries
+RELAX_USES_LIBRARY_CHECK := true
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
