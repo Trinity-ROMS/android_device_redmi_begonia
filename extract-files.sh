7783e3cb9b80c014d52af6bede2b4c99c7d8dc22
nift4 <nift4@protonmail.com>
2023-12-07 22:38:35 +0100

    begonia: Shim beanpod keymaster
    _ZN9keymaster19GenerateKeyResponseD1Ev is the destructor of the class.
    It got removed in 12, so it won't hurt empty-shimming it.
    
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -106,6 +106,9 @@ function blob_fixup {
         vendor/lib64/libcam.utils.sensorprovider.so)
             "${PATCHELF}" --replace-needed "libsensorndkbridge.so" "libsensorndkbridge-v30.so" "${2}"
             ;;
+        vendor/bin/hw/android.hardware.keymaster@4.0-service.beanpod)
+            "${PATCHELF}" --add-needed "libshim_beanpod.so" "${2}"
+            ;;
     esac
 }
 
