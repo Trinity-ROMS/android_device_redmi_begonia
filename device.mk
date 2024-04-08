54b0da563e4be3773d44851364f7a4524f36e5ff
bengris32 <bengris32@protonmail.ch>
2024-04-08 17:20:03 +0000

    begonia: Build wpa_supplicant from source
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Change-Id: Ic52912b5a056ddf8f044ba14dd894978692e8f68

--- device.mk
+++ device.mk
@@ -419,6 +419,7 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_PACKAGES += \
     hostapd \
+    wpa_supplicant \
     libwifi-hal-wrapper \
     android.hardware.wifi@1.0.vendor \
     android.hardware.wifi@1.1.vendor \
@@ -426,10 +427,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.3.vendor \
     android.hardware.wifi@1.4.vendor \
     android.hardware.wifi@1.5.vendor \
-    android.hardware.wifi.supplicant@1.0.vendor \
-    android.hardware.wifi.supplicant@1.1.vendor \
-    android.hardware.wifi.supplicant@1.2.vendor \
-    android.hardware.wifi.supplicant@1.3.vendor
+    WifiResOverlay
 
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service-lazy
