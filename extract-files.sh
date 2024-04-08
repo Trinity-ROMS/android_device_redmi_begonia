7a4e8678cc1ad6abfe2b18bea3b2d9cc86f504f2
SamarV-121 <samar@samarv121.dev>
2024-04-08 17:26:37 +0000

    begonia: Rename libwifi-hal.so to prevent conflict with AOSP
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>
    Change-Id: Ia75aae1533b0f3d1cb759f21137f416fdeca9123

--- extract-files.sh
+++ extract-files.sh
@@ -109,6 +109,9 @@ function blob_fixup {
         vendor/bin/hw/android.hardware.keymaster@4.0-service.beanpod)
             "${PATCHELF}" --add-needed "libshim_beanpod.so" "${2}"
             ;;
+        vendor/lib64/libwifi-hal-mtk.so)
+            "${PATCHELF}" --set-soname "libwifi-hal-mtk.so" "${2}"
+            ;;
         lib64/libem_support_jni.so)
             "${PATCHELF}" --add-needed "libjni_shim.so" "${2}"
             ;;
