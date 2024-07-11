e9c52ebc42246164d6b795c8d552129a94049067
Saikrishna1504 <saikrishna26918@gmail.com>
2024-07-11 09:43:39 +0530

    begonia: Also patch libMtkOmxVdecEx.so to load the old graphicsbuffer symbol
    E osal_utils: dlopen failed, dlopen failed: cannot locate symbol "_ZN7android19GraphicBufferMapper4lockEPK13native_handlejRKNS_4RectEPPvPiS9_" referenced by "/vendor/lib/libMtkOmxVdecEx.so"...
    
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -493,6 +493,7 @@ PRODUCT_PACKAGES += \
 
 # Libui shim
 PRODUCT_PACKAGES += \
+    libui_shim \
     libui_shim.vendor
 
 # Inherit vendor
--- extract-files.sh
+++ extract-files.sh
@@ -115,6 +115,7 @@ function blob_fixup {
         lib64/libem_support_jni.so)
             "${PATCHELF}" --add-needed "libjni_shim.so" "${2}"
             ;;
+        vendor/lib/libMtkOmxVdecEx.so|\
         lib/libsource.so)
             grep -q libui_shim.so "$2" || "$PATCHELF" --add-needed libui_shim.so "$2"
             ;;
